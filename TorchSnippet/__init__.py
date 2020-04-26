b1c500d9412926db9a093be6ff26e8f759a8697f
yantijin <yantijin@163.com>
2020-04-26 20:34:24 +0800

    modify import

--- TorchSnippet/__init__.py
+++ TorchSnippet/__init__.py
@@ -1,9 +1,8 @@
+from . import Layers
+from . import Flow
+from . import ode
 from .arg_check import *
 from .core import *
 from .typing_ import *
-from .Layers import *
-from .Flow import *
-from .ode import *
-
 
 __version__ = '0.0.1'
